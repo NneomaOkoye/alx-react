715563dcc942176e11bf1f590bbe7ee3b00f4fea
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-11-03 00:01:25 +0100

    Update App.test.js
    completed

--- 0x03-React_component/task_0/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.test.js
@@ -1,5 +1,10 @@
 import App from './App';
 import { shallow } from 'enzyme';
+import Notifications from '../Notifications/Notifications';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import CourseList from '../CourseList/CourseList';
 
 describe('App', () => {
   it('renders without crashing', () => {
@@ -8,23 +13,42 @@ describe('App', () => {
   });
 });
 
-// describe('App- header', () => {
-//   it('renders a div with class App-header', () => {
-//     const wrapper = shallow(<App />);
-//     expect(wrapper.find('.App-header')).toHaveLength(1);
-//   });
-// });
+describe('App component - Notification', () => {
+  it('should render the Notifications component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Notifications)).toHaveLength(1);
+  });
+});
 
-// describe('App-body', () => {
-//   it('renders a div with class App-body', () => {
-//     const wrapper = shallow(<App />);
-//     expect(wrapper.find('.App-body')).toHaveLength(1);
-//   });
-// });
+describe('App component - Header', () => {
+  it('should render the Header component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Header)).toHaveLength(1);
+  });
+});
+describe('App component - login', () => {
+  it('should render the Login component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Login)).toHaveLength(1);
+  });
+});
 
-// describe('App-footer', () => {
-//   it('renders a div with class App-footer', () => {
-//     const wrapper = shallow(<App />);
-//     expect(wrapper.find('.App-footer')).toHaveLength(1);
-//   });
-// });
+describe('App component - Footer', () => {
+  it('should render the Footer component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Footer)).toHaveLength(1);
+  });
+});
+
+describe('App component', () => {
+  it('does not render Courselist if not logged in', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.contains(<CourseList />)).toBe(false);
+    expect(wrapper.contains(<Login />)).toBe(true);
+  });
+  it('renders courselist if logged in', () => {
+    const wrapper = shallow(<App isLoggedIn={true} />);
+    expect(wrapper.contains(<CourseList />)).toBe(true);
+    expect(wrapper.contains(<Login />)).toBe(false);
+  });
+});
