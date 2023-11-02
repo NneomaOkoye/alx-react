261a2570a039b2c7684c45c8c3f019d59ec7c5e0
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-11-03 00:00:31 +0100

    Update App.js
    completed

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,27 +1,67 @@
 import React from 'react';
-import { getFooterCopy, getFullYear } from '../utils/utils';
+import { getLatestNotification } from '../utils/utils';
 import Header from '../Header/Header';
 import './App.css';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
 import Notifications from '../Notifications/Notifications';
+import PropTypes from 'prop-types';
+import CourseList from '../CourseList/CourseList';
 
-function App() {
-  const currentYear = getFullYear();
-  const footer = getFooterCopy(true);
-  return (
-    <>
-      <Notifications />
-      <div className='App'>
-        <Header />
-        <div className='App-body'>
-          <Login />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listCourses: [
+        {
+          id: 1,
+          name: 'ES6',
+          credit: 60,
+        },
+        {
+          id: 2,
+          name: 'Webpack',
+          credit: 20,
+        },
+        {
+          id: 3,
+          name: 'React',
+          credit: 40,
+        },
+      ],
+
+      listNotifications: [
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        { id: 3, type: 'default', html: getLatestNotification() },
+      ],
+    };
+  }
+
+  render() {
+    const { listCourses, listNotifications } = this.state;
+    const isLoggedIn = this.props.isLoggedIn;
+    return (
+      <>
+        <Notifications listNotifications={listNotifications} />
+        <div className='App'>
+          <Header />
+          <div className='App-body'>
+            {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          </div>
+          <Footer />
         </div>
-
-        <Footer />
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
 
 export default App;
