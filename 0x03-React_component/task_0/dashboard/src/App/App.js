380d16a2a88e916145045b2b9688c1c8f0113a79
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-11-03 01:27:03 +0100

    Update App.js
    done

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,67 +1,23 @@
-import React from 'react';
-import { getLatestNotification } from '../utils/utils';
-import Header from '../Header/Header';
-import './App.css';
-import Login from '../Login/Login';
-import Footer from '../Footer/Footer';
-import Notifications from '../Notifications/Notifications';
-import PropTypes from 'prop-types';
-import CourseList from '../CourseList/CourseList';
+import logo from './holberton-logo.jpg';
+import './App.css'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      listCourses: [
-        {
-          id: 1,
-          name: 'ES6',
-          credit: 60,
-        },
-        {
-          id: 2,
-          name: 'Webpack',
-          credit: 20,
-        },
-        {
-          id: 3,
-          name: 'React',
-          credit: 40,
-        },
-      ],
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
 
-      listNotifications: [
-        { id: 1, type: 'default', value: 'New course available' },
-        { id: 2, type: 'urgent', value: 'New resume available' },
-        { id: 3, type: 'default', html: getLatestNotification() },
-      ],
-    };
-  }
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
 
-  render() {
-    const { listCourses, listNotifications } = this.state;
-    const isLoggedIn = this.props.isLoggedIn;
-    return (
-      <>
-        <Notifications listNotifications={listNotifications} />
-        <div className='App'>
-          <Header />
-          <div className='App-body'>
-            {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-          </div>
-          <Footer />
-        </div>
-      </>
-    );
-  }
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
+    </div>
+  );
 }
-
-App.propTypes = {
-  isLoggedIn: PropTypes.bool,
-};
-
-App.defaultProps = {
-  isLoggedIn: false,
-};
 
 export default App;
