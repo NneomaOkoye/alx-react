2da5c2cb649b9a19624305f3d6056f286f04bcde
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-11-08 03:26:40 +0100

    Update CourseList.js
    done

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseList.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseList.js
@@ -1,39 +1,33 @@
-import React from 'react';
-import CourseListRow from './CourseListRow';
-import './CourseList.css';
-import PropTypes from 'prop-types';
-import { CourseShape } from './CourseShape';
+import React from "react";
+import "./CourseList.css";
+import CourseListRow from "./CourseListRow";
+import PropTypes from "prop-types";
+import CourseShape from "./CourseShape";
 
-const CourseList = ({ listCourses }) => {
+function CourseList({ listCourses }) {
   return (
-    <table id='CourseList'>
+    <table id="CourseList">
       <thead>
-        <CourseListRow textFirstCell='Available courses' isHeader={true} />
-        <CourseListRow
-          textFirstCell='Course name'
-          textSecondCell='Credit'
-          isHeader={true}
-        />
+        <CourseListRow textFirstCell="Available courses" isHeader={true} />
+        <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
       </thead>
       <tbody>
-        {listCourses?.length === 0 ? (
-          <CourseListRow textFirstCell={'No course available yet'} />
+        {listCourses.length > 0 ? (
+          listCourses.map(({ id, name, credit }) => <CourseListRow key={id} textFirstCell={name} textSecondCell={credit} />)
         ) : (
-          listCourses?.map(({ id, name, credit }) => (
-            <CourseListRow
-              key={id}
-              textFirstCell={name}
-              textSecondCell={credit}
-            />
-          ))
+          <CourseListRow textFirstCell="No course available yet" />
         )}
       </tbody>
     </table>
   );
-};
+}
 
 CourseList.propTypes = {
   listCourses: PropTypes.arrayOf(CourseShape),
+};
+
+CourseList.defaultProps = {
+  listCourses: [],
 };
 
 export default CourseList;
