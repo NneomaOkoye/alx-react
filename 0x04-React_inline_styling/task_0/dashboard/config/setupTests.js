8ebc803fefea08b3305092ed8c79834d6386e4b3
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-11-08 03:56:25 +0100

    Update setupTests.js
    done

--- 0x04-React_inline_styling/task_0/dashboard/config/setupTests.js
+++ 0x04-React_inline_styling/task_0/dashboard/config/setupTests.js
@@ -1,4 +1,42 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-configure({ adapter: new Adapter() });
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './dist/index.html',
+    }),
+  ],
+  devServer: {
+    hot: true,
+  },
+  devtool: 'inline-source-map',
+};
