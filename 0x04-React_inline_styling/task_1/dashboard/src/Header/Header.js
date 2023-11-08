8ed29b1565a3589e96d2c6307be07a898d02ae46
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-11-08 05:11:02 +0100

    Update Header.js
    done

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import './Header.css';
+import {StyleSheet, css} from 'aphrodite';
 
 const Header = () => {
   return (
-    <div className='App-header'>
+    <div className={css(styles.AppHeader)}>
       <h1>School dashboard</h1>
     </div>
   );
 };
+
+const styles = StyleSheet.create({
+  AppHeader : {
+    backgroundImage: "url('../assets/logo.jpg')",
+  backgroundPosition: 'left',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: '30% 100%',
+  width: '100%',
+  height: '15rem',
+  display: 'flex',
+  alignItems: 'center',
+  paddingLeft: '31%',
+  color: '#c7254e',
+  }
+})
 
 export default Header;
