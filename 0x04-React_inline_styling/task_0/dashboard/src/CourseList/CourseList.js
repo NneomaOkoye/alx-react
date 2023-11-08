50985316a346a51ae923bdb18d24653f42686436
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-11-08 03:45:39 +0100

    Update CourseList.js
    done

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseList.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseList.js
@@ -1,26 +1,51 @@
-import React from "react";
-import "./CourseList.css";
-import CourseListRow from "./CourseListRow";
-import PropTypes from "prop-types";
-import CourseShape from "./CourseShape";
+import React from 'react';
+import PropTypes from 'prop-types';
+import CourseListRow from './CourseListRow';
+import CourseShape from './CourseShape';
+import { StyleSheet, css } from 'aphrodite';
 
-function CourseList({ listCourses }) {
+const CourseList = ({ listCourses }) => {
+
+  const styles = StyleSheet.create({
+    table: {
+      width: '100%',
+      border: '1px solid #ccc'
+  },
+  thead: {
+      textAlign: 'center'
+    },
+  
+    th: {
+      textAlign: 'center',
+      borderBottom: '2px solid #ccc'
+    }
+   
+  
+  
+  })
   return (
-    <table id="CourseList">
-      <thead>
-        <CourseListRow textFirstCell="Available courses" isHeader={true} />
+    <table id="CourseList" className={css(styles.table)}>
+      <thead className={css(styles.th)}>
+        <CourseListRow textFirstCell="Available courses" isHeader={true} className={css(styles.thead)}/>
         <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
       </thead>
       <tbody>
-        {listCourses.length > 0 ? (
-          listCourses.map(({ id, name, credit }) => <CourseListRow key={id} textFirstCell={name} textSecondCell={credit} />)
+        {listCourses.length === 0 ? (
+          <CourseListRow textFirstCell="No course available yet" isHeader={false} />
         ) : (
-          <CourseListRow textFirstCell="No course available yet" />
+          listCourses.map(course => (
+            <CourseListRow
+              key={course.id}
+              textFirstCell={course.name}
+              textSecondCell={course.credit}
+              isHeader={false}
+            />
+          ))
         )}
       </tbody>
     </table>
   );
-}
+};
 
 CourseList.propTypes = {
   listCourses: PropTypes.arrayOf(CourseShape),
