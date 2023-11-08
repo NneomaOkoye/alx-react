ef59b40c88b5ee304e97359f2f577278356296f4
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-11-08 03:48:03 +0100

    Update CourseShape.js
    done

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseShape.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseShape.js
@@ -4,6 +4,6 @@ const CourseShape = PropTypes.shape({
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   credit: PropTypes.number.isRequired,
-});
+})
 
 export default CourseShape;
