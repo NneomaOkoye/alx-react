e96fd1fdd938ec231432e7dc0c6424a98e17569a
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-11-23 09:20:55 +0100

    Update CourseListRow.js
    done

--- 0x07-react_redux_action_creator_normalizr/task_2/dashboard/src/CourseList/CourseListRow.js
+++ 0x07-react_redux_action_creator_normalizr/task_2/dashboard/src/CourseList/CourseListRow.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import {StyleSheet, css} from 'aphrodite';
+import { StyleSheet, css } from 'aphrodite';
 
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
   const style = { backgroundColor: isHeader ? '#deb5b545' : '#f5f5f5ab' };
+  const [isChecked, setIschecked] = useState(false);
+
   return (
     <tr style={style}>
       {isHeader ? (
@@ -17,7 +19,14 @@ const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
         )
       ) : (
         <>
-          <td>{textFirstCell}</td>
+          <td className={css(isChecked && styles.rowChecked)}>
+            <input
+              type='checkbox'
+              value={isChecked}
+              onChange={() => setIschecked((prev) => !prev)}
+            />
+            {textFirstCell}
+          </td>
           <td>{textSecondCell}</td>
         </>
       )}
@@ -37,13 +46,12 @@ CourseListRow.defaultProps = {
 };
 
 const styles = StyleSheet.create({
-  default: {
-    
+  default: {},
+
+  header: {},
+  rowChecked: {
+    backgroundColor: '#e6e4e4',
   },
-  
-  header : {
-    
-  }
-})
+});
 
 export default CourseListRow;
