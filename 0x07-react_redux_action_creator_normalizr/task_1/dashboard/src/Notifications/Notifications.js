b22494919d24515e8727f97a4b43d54a711a4563
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-11-23 08:30:44 +0100

    Update Notifications.js
    done

--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/Notifications/Notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/Notifications/Notifications.js
@@ -1,50 +1,48 @@
 import React, { Component } from 'react';
 import closeIcon from '../assets/close-icon.png';
-import './Notifications.css';
-import {StyleSheet,css} from 'aphrodite';
-import { getLatestNotification } from '../utils/utils';
+import { StyleSheet, css } from 'aphrodite';
 import NotificationItem from './NotificationItem';
 import PropTypes from 'prop-types';
 import { NotificationItemShape } from './NotificationItemShape';
 
 class Notifications extends Component {
-  markAsRead = (id) => {
-    console.log(`Notification ${id} has been marked as read`);
-  };
-
-  shouldComponentUpdate = (nextProps) => {
-    return (
-      nextProps.listNotifications.length > this.props.listNotifications.length
-    );
-  };
-
   render() {
-    const { displayDrawer, listNotifications } = this.props;
+    const {
+      displayDrawer,
+      listNotifications,
+      handleDisplayDrawer,
+      handleHideDrawer,
+      markNotificationAsRead,
+    } = this.props;
     return (
       <>
-        <div className='menuItem'>
-          <p>Your Notifications</p>
+        <div className={css(styles.menuItem)} onClick={handleDisplayDrawer}>
+          <button className={css(styles.p)} onClick={handleDisplayDrawer}>
+            Your Notifications
+          </button>
         </div>
         {displayDrawer && (
           <div className={css(styles.Notifications)}>
             <button
               type='button'
               aria-label='Close'
-              onClick={() => {
-                console.log('Close button has been clicked');
-              }}
+              onClick={handleHideDrawer}
               className={css(styles.closeBtn)}
             >
-              <img src={closeIcon} alt='close icon' className={css(styles.img)} />
+              <img
+                src={closeIcon}
+                alt='close icon'
+                className={css(styles.img)}
+              />
             </button>
-            <p>Here is the list of notifications</p>
+            <p className={css(styles.p)}>Here is the list of notifications</p>
             <ul className={css(styles.ul)}>
               {listNotifications?.length > 0 ? (
                 listNotifications.map((item) => (
                   <NotificationItem
                     key={item.id}
                     {...item}
-                    markAsRead={this.markAsRead}
+                    markAsRead={markNotificationAsRead}
                   />
                 ))
               ) : (
@@ -61,37 +59,94 @@ class Notifications extends Component {
 Notifications.propTypes = {
   displayDrawer: PropTypes.bool,
   listNotifications: PropTypes.arrayOf(NotificationItemShape),
+  handleDisplayDrawer: PropTypes.func,
+  handleHideDrawer: PropTypes.func,
+  markNotificationAsRead: PropTypes.func,
 };
 
 Notifications.defaultProps = {
   displayDrawer: false,
+  handleDisplayDrawer: () => {},
+  handleHideDrawer: () => {},
+};
+
+const fadeIn = {
+  from: {
+    opacity: 0.5,
+  },
+  to: {
+    opacity: 1,
+  },
+};
+
+const bounce = {
+  '0%': {
+    transform: 'translateY(0)',
+  },
+  '25%': {
+    transform: 'translateY(-5px)',
+  },
+  '50%': {
+    transform: 'translateY(0)',
+  },
+  '75%': {
+    transform: 'translateY(5px)',
+  },
+  '100%': {
+    transform: 'translateY(0)',
+  },
 };
 
 const styles = StyleSheet.create({
-  Notifications : {
-  border: '2px dotted #c7254e',
-  padding: '1rem',
-  position: 'absolute',
-  right: 0,
-  width: '30%',
-},
+  Notifications: {
+    border: '2px dotted #c7254e',
+    padding: '1rem',
+    position: 'absolute',
+    right: 0,
+    width: '30%',
+    '@media (max-width: 600px)': {
+      width: '100%',
+      position: 'fixed',
+      top: 0,
+      bottom: 0,
+      border: 0,
+      borderBottom: '1px solid black',
+      zIndex: '50',
+      backgroundColor: 'white',
+      boxSizing: 'border-box',
+    },
+  },
 
-closeBtn : {
-  background: 'none',
-  border: 'none',
-  float: 'right',
-  textAlign: 'right',
-},
+  closeBtn: {
+    background: 'none',
+    border: 'none',
+    float: 'right',
+    textAlign: 'right',
+    cursor: 'pointer',
+  },
 
-img : {
-  width: '10%'
-},
+  img: {
+    width: '10%',
+  },
 
-ul : {
-  paddingTop: '0.7rem',
-  paddingLeft: '3rem',
-},
+  ul: {
+    paddingTop: '0.7rem',
+    paddingLeft: '3rem',
+  },
+  p: {
+    fontWeight: 600,
+    fontSize: '1.1rem',
+  },
 
-})
+  menuItem: {
+    textAlign: 'right',
+    ':hover': {
+      cursor: 'pointer',
+      animationName: [fadeIn, bounce],
+      animationIterationCount: 3,
+      animationDuration: '0.5s',
+    },
+  },
+});
 
 export default Notifications;
