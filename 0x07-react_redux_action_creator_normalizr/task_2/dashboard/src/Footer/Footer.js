d8bec3d8df97e5799a422bdc6a936824806a00d7
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-11-23 09:22:55 +0100

    Update Footer.js
    done

--- 0x07-react_redux_action_creator_normalizr/task_2/dashboard/src/Footer/Footer.js
+++ 0x07-react_redux_action_creator_normalizr/task_2/dashboard/src/Footer/Footer.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Footer.css';
 import { getFooterCopy, getFullYear } from '../utils/utils';
+import { UserContext } from '../App/AppContext';
 
 const Footer = () => {
+  const { user } = useContext(UserContext);
   const currentYear = getFullYear();
   const footer = getFooterCopy(true);
   return (
     <div className='App-footer'>
       <p>{`Copyright ${currentYear} - ${footer}`}</p>
+      {user.isLoggedIn && (
+        <p>
+          <a href='/'>Contact us</a>
+        </p>
+      )}
     </div>
   );
 };
