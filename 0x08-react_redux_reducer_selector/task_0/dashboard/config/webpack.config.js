aa4b0e0020b511d619ffbc77b550f764cd32c49c
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-11-30 03:40:21 +0100

    Update webpack.config.js
    done

--- 0x08-react_redux_reducer_selector/task_0/dashboard/config/webpack.config.js
+++ 0x08-react_redux_reducer_selector/task_0/dashboard/config/webpack.config.js
@@ -1,54 +1,57 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+    devtool: 'inline-source-map',
+    entry: path.resolve(__dirname, '/src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: './deploy',
+        open: true,
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000,
+        hot: true,
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        // type: 'asset/resource',
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-  devServer: {
-    static: './dist',
-    compress: true,
-    open: true,
-    hot: true,
-    port: 8564,
-  },
-  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      name: 'index.html',
-      inject: false,
-      template: './dist/index.html',
+      title: "Webpack Output",
     }),
   ],
+  module: {
+    rules: [
+        ...
+        { 
+          test: /\.css$/, 
+          use: ["style-loader", "css-loader"] 
+        
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+            test: /\.(gif|png|jpe?g|svg)$/i,
+            use: [
+              'file-loader',
+              {
+                loader: 'image-webpack-loader',
+                options: {
+                  bypassOnDebug: true,
+                  disable: true,
+                },
+              },
+            ],
+          },
+        
+       
+      ]
+  },
+
 };
