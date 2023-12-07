94b0648422ba32acbe57bc154cee17a1e38c10b3
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-12-07 04:55:05 +0100

    Update utils.test.js
    done

--- 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/utils/utils.test.js
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/utils/utils.test.js
@@ -1,26 +1,29 @@
-import { getFooterCopy, getFullYear, getLatestNotification } from './utils';
 
-describe('getFooterCopy', () => {
-  it('should return the appropriate footer text', () => {
-    const footer = getFooterCopy(true);
-    const footerTwo = getFooterCopy(false);
-    expect(footer).toBe('Holberton School');
-    expect(footerTwo).toBe('Holberton School main dashboard');
+import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
+
+describe('getFullYear function', () => {
+  it('should return the correct year', () => {
+    const currentYear = new Date().getFullYear();
+    expect(getFullYear()).toEqual(currentYear);
   });
 });
 
-describe('getFullYear', () => {
-  it('should return the current year', () => {
-    const date = new Date();
-    const currentYear = getFullYear();
-    expect(currentYear).toBe(date.getFullYear());
+describe('getFooterCopy function', () => {
+  it('should return the correct string for argument true', () => {
+    const copyForTrue = 'All rights reserved ©';
+    expect(getFooterCopy(true)).toEqual(copyForTrue);
+  });
+
+  it('should return the correct string for argument false', () => {
+    const copyForFalse = 'Visit us at www.example.com';
+    expect(getFooterCopy(false)).toEqual(copyForFalse);
   });
 });
 
-describe('getLatestNotification', () => {
-  it('should return the appropriate string', () => {
-    const str = '<strong>Urgent requirement</strong> - complete by EOD';
-    const notif = getLatestNotification();
-    expect(notif).toBe(str);
+describe('getLatestNotification function', () => {
+  it('should return the correct string', () => {
+    const expectedNotification =
+      '<strong>Urgent requirement</strong> - complete by EOD';
+    expect(getLatestNotification()).toEqual(expectedNotification);
   });
 });
