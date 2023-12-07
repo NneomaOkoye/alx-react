f55e4a42464eaa2c5b3b59849a36d83f24e81a2b
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-12-07 04:38:17 +0100

    Update Notifications.test.js
    done

--- 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/Notifications/Notifications.test.js
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/Notifications/Notifications.test.js
@@ -1,23 +1,70 @@
-import { shallow } from 'enzyme';
+import React from 'react';
 import Notifications from './Notifications';
+import { shallow } from 'enzyme';
+import NotificationItem from './NotificationItem';
 
-describe('Notifications', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<Notifications />);
-    expect(wrapper.find('.Notifications')).toHaveLength(1);
-  });
-});
+describe('<Notififactions />', () => {
+    it ('renders without crashing', () => {
+        shallow(<Notifications />);
+    })
 
-describe('Notifications list', () => {
-  it('renders three list items', () => {
-    const wrapper = shallow(<Notifications />);
-    expect(wrapper.find('li')).toHaveLength(3);
-  });
-});
+    it('the component renders NotificationItem elements', () => {
+        const wrapper = shallow(<Notificationitem />);
+        expect(wrapper.find(NotificationItem)).toHaveLength(1);
+    })
+    it('renders the text Here is the list of notifications', () => {
+        const text = 'Here is the list of notifications';
+        const wrapper = shallow(<Notification />);
+        expect(wrapper.find('p').text()).toBe(text);
+    })
 
-describe('Notifications text', () => {
-  it('renders correct text', () => {
-    const wrapper = shallow(<Notifications />);
-    expect(wrapper.text()).toContain('Here is the list of notifications');
-  });
-});
+    it('renders NotificationItem elements', () => {
+        const dummyNotifications = [
+          { type: 'default', html: { __html: 'Test 1' }, value: 'Test 1' },
+          { type: 'info', html: { __html: 'Test 2' }, value: 'Test 2' },
+          { type: 'success', html: { __html: 'Test 3' }, value: 'Test 3' },
+        ];
+        const wrapper = shallow(<Notifications notifications={dummyNotifications} />);
+        const notificationItems = wrapper.find(NotificationItem);
+        expect(notificationItems.length).toBe(dummyNotifications.length);
+      });
+
+      it('renders the first NotificationItem element with the right HTML', () => {
+        const dummyNotifications = [
+          { type: 'default', html: { __html: '<b>Test 1</b>' }, value: 'Test 1' },
+          { type: 'info', html: { __html: '<i>Test 2</i>' }, value: 'Test 2' },
+          { type: 'success', html: { __html: '<u>Test 3</u>' }, value: 'Test 3' },
+        ];
+    
+        const wrapper = shallow(<Notifications notifications={dummyNotifications} />);
+        const notificationItems = wrapper.find(NotificationItem);
+        expect(notificationItems.length).toBe(dummyNotifications.length);
+    
+        const firstNotificationItem = notificationItems.first();
+        expect(firstNotificationItem.prop('html')).toEqual({ __html: '<b>Test 1</b>' });
+      });
+    });
+    describe('Notifications', () => {
+      it('should call handleDisplayDrawer when menu item is clicked', () => {
+        const wrapper = shallow(<Notifications />);
+        const instance = wrapper.instance();
+        const handleDisplayDrawerSpy = jest.spyOn(instance, 'handleDisplayDrawer');
+    
+        // Simulate a click on the menu item
+        wrapper.find('.menu-item').simulate('click');
+    
+        expect(handleDisplayDrawerSpy).toHaveBeenCalled();
+      });
+    
+      it('should call handleHideDrawer when button is clicked', () => {
+        const wrapper = shallow(<Notifications />);
+        const instance = wrapper.instance();
+        const handleHideDrawerSpy = jest.spyOn(instance, 'handleHideDrawer');
+    
+        // Simulate a click on the button
+        wrapper.find('.hide-button').simulate('click');
+    
+        expect(handleHideDrawerSpy).toHaveBeenCalled();
+      });
+    });
+    
