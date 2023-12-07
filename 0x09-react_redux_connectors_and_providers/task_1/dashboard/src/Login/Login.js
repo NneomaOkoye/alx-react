c164c2a230bbd29351aca12a39b93331a20834d7
Nneoma Okoye <117756776+NneomaOkoye@users.noreply.github.com>
2023-12-07 04:34:45 +0100

    Update Login.js
    done

--- 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/Login/Login.js
+++ 0x09-react_redux_connectors_and_providers/task_1/dashboard/src/Login/Login.js
@@ -1,22 +1,88 @@
-import React from 'react';
-import './Login.css';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, css } from 'aphrodite';
 
-const Login = () => {
+export default function Login(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [enableSubmit, setEnableSubmit] = useState(false);
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    props.logIn(
+      e.target.elements.email.value,
+      e.target.elements.password.value
+    );
+  };
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  useEffect(() => {
+    if (email !== '' && password !== '') {
+      setEnableSubmit(true);
+    } else {
+      if (enableSubmit !== false) {
+        setEnableSubmit(false);
+      }
+    }
+  }, [email, password]);
+
   return (
-    <>
+    <div className={css(styleLogin.appbody)}>
       <p>Login to access the full dashboard</p>
-      <label htmlFor='email'>
-        Email
-        <input type='email' id='email' name='email' />
-      </label>
+      <form onSubmit={handleLoginSubmit}>
+        <div className={css(styleLogin.inputWrapper)}>
+          <label className={css(styleLogin.screen)} htmlFor='email'>
+            Email:
+          </label>
+        </div>
 
-      <label htmlFor='password'>
-        Password
-        <input type='password' name='password' id='password' />
-      </label>
-      <button type='submit'>OK</button>
-    </>
+        <input
+          className={css(styleLogin.inputlogin)}
+          type='email'
+          name='email'
+          id='email'
+          value={email}
+          onChange={handleChangeEmail}
+        ></input>
+        <div className={css(styleLogin.inputWrapper)}>
+          <label htmlFor='password'>Password:</label>
+        </div>
+
+        <input
+          className={css(styleLogin.inputlogin.screen)}
+          type='password'
+          name='password'
+          id='password'
+          value={password}
+          onChange={handleChangePassword}
+        ></input>
+        <div className={css(styleLogin.inputWrapper)}>
+          <input type='submit' value='OK' disabled={!enableSubmit}></input>
+        </div>
+      </form>
+    </div>
   );
-};
+}
 
-export default Login;
+const styleLogin = StyleSheet.create({
+  appbody: {
+    textAlign: 'left',
+    margin: '50px',
+  },
+
+  inputlogin: {
+    marginRight: 10,
+  },
+
+  '@media (max-width 900px)': {
+    inputWrapper: {
+      display: 'block',
+    },
+  },
+});
